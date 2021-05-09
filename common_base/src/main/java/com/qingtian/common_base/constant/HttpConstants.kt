ce5b5672f2d78f3dc031c8af2ce99784ebb41236
huangqingtian888 <18377613923@163.com>
2021-05-09 20:23:00 +0800

    模块：common_base模块 内容：common_bases

---
+++ common_base/src/main/java/com/qingtian/common_base/constant/HttpConstants.kt
@@ -0,0 +1,17 @@
+package com.qingtian.common_base.constant
+
+/**
+ * create by hqt on 2021/5/9 20:13
+ * desc:
+ */
+
+class HttpConstants {
+
+
+    companion object {
+
+        val BASE_URL : String = "https://www.wanandroid.com"
+    }
+
+
+}
